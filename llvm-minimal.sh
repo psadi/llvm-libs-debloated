0a67aff602fb0247eb37160acde6f9cedb61b481
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-01-29 23:06:16 +0000

    Add files via upload

---
+++ llvm-minimal.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+git clone https://gitlab.archlinux.org/archlinux/packaging/packages/llvm llvm
+cd ./llvm
+
+sed -i -e 's/-g1/-g0/' \
+	-e 's|-DCMAKE_BUILD_TYPE=Release|-DCMAKE_BUILD_TYPE=MinSizeRel|' \
+	-e 's|-DLLVM_BUILD_TESTS=ON|-DLLVM_BUILD_TESTS=OFF|' \
+	-e 's|-DLLVM_ENABLE_CURL=ON|-DLLVM_ENABLE_CURL=OFF|' \
+	-e 's|-DLLVM_ENABLE_SPHINX=ON|-DLLVM_ENABLE_SPHINX=OFF|' \
+	-e 's|rm -r|#rm -r|' ./PKGBUILD
+cat ./PKGBUILD
+
+makepkg -f --skippgpcheck
+mv ./llvm-libs-*.pkg.tar.zst ../llvm-libs-minimal.pkg.tar.zst
+cd ..
+echo "All done!"
+
