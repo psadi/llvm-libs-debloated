df2a11429a79a3b7f0685ab2ac4fc777b9b6c0c1
psadi <ps.adithya@icloud.com>
2025-02-12 10:53:30 +0530

    ci: add support for arm build

--- llvm-minimal.sh
+++ llvm-minimal.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+ARCH="$(uname -m)"
+
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/llvm llvm
 cd ./llvm
 
 sed -i -e 's/-g1/-g0/' \
+	-e "s/x86_64/${ARCH}/" \
 	-e 's|-DCMAKE_BUILD_TYPE=Release|-DCMAKE_BUILD_TYPE=MinSizeRel|' \
 	-e 's|-DLLVM_BUILD_TESTS=ON|-DLLVM_BUILD_TESTS=OFF|' \
 	-e 's|-DLLVM_ENABLE_CURL=ON|-DLLVM_ENABLE_CURL=OFF|' \
@@ -14,6 +17,7 @@ sed -i -e 's/-g1/-g0/' \
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
+ls -la
 mv ./llvm-libs-*.pkg.tar.zst ../llvm-libs-minimal.pkg.tar.zst
 cd ..
 rm -rf ./llvm
--- llvm-very-minimal.sh
+++ llvm-very-minimal.sh
@@ -2,19 +2,37 @@
 
 set -e
 
+export ARCH="$(uname -m)"
+
+case "${ARCH}" in
+"x86_64")
+	TARGETS_TO_BUILD="X86;AMDGPU"
+	;;
+"aarch64")
+	TARGETS_TO_BUILD="AArch64;AMDGPU"
+	;;
+*)
+	echo -e "Unsupported Arch: '${ARCH}'"
+	exit 1
+	;;
+esac
+
 git clone https://gitlab.archlinux.org/archlinux/packaging/packages/llvm llvm
 cd ./llvm
 
 sed -i -e 's/-g1/-g0/' \
+	-e "s/x86_64/${ARCH}/" \
 	-e 's|-DCMAKE_BUILD_TYPE=Release|-DCMAKE_BUILD_TYPE=MinSizeRel|' \
 	-e 's|-DLLVM_BUILD_TESTS=ON|-DLLVM_BUILD_TESTS=OFF|' \
 	-e 's|-DLLVM_ENABLE_CURL=ON|-DLLVM_ENABLE_CURL=OFF|' \
-	-e 's|-DLLVM_BUILD_DOCS=ON|-DLLVM_TARGETS_TO_BUILD="X86;AMDGPU"|' \
+	-e "s|-DLLVM_BUILD_DOCS=ON|-DLLVM_TARGETS_TO_BUILD=\"${TARGETS_TO_BUILD}\"|" \
 	-e 's|-DLLVM_ENABLE_SPHINX=ON|-DLLVM_ENABLE_SPHINX=OFF|' \
 	-e 's|rm -r|#rm -r|' ./PKGBUILD
+
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
+ls -la
 mv ./llvm-libs-*.pkg.tar.zst ../llvm-libs-very-minimal.pkg.tar.zst
 cd ..
 rm -rf ./llvm
