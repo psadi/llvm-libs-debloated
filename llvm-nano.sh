83ae03c8e0ba3a43b1e185f5040c832b12aaa468
psadi <ps.adithya@icloud.com>
2025-02-13 14:22:30 +0530

    ci: build & release

--- llvm-nano.sh
+++ llvm-nano.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ARCH="$(uname -m)"
+export ARCH="$(uname -m)"
 
 case "${ARCH}" in
 "x86_64")
@@ -27,13 +27,15 @@ sed -i -e 's/-g1/-g0/' \
 	-e 's|-DCMAKE_BUILD_TYPE=Release|-DCMAKE_BUILD_TYPE=MinSizeRel|' \
 	-e 's|-DLLVM_BUILD_TESTS=ON|-DLLVM_BUILD_TESTS=OFF|' \
 	-e 's|-DLLVM_ENABLE_CURL=ON|-DLLVM_ENABLE_CURL=OFF|' \
+	-e "s|-DLLVM_BUILD_DOCS=ON|-DLLVM_TARGETS_TO_BUILD=\"${TARGETS_TO_BUILD}\"|" \
 	-e 's|-DLLVM_ENABLE_SPHINX=ON|-DLLVM_ENABLE_SPHINX=OFF|' \
 	-e 's|rm -r|#rm -r|' ./PKGBUILD
+
 cat ./PKGBUILD
 
 makepkg -f --skippgpcheck
 ls -la
-mv ./llvm-libs-*.pkg.tar.${EXT} ../llvm-libs-minimal.pkg.tar.${EXT}
+mv ./llvm-libs-*.pkg.tar.${EXT} ../llvm-libs-nano-${ARCH}.pkg.tar.${EXT}
 cd ..
 rm -rf ./llvm
 echo "All done!"
